17aabab82309781832c0dd834cca4702513a40a4
Dhia22 <47751098+Dhia22@users.noreply.github.com>
2019-02-19 21:50:39 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,3 +14,22 @@ python3 -B main.py -t --batch_size 70 --epochs 200 --base_learning_rate=0.001 --
 !wget http://41.229.96.242/uploads/datag.tar.gz
 !tar -v -xvzf datag.tar.gz
 !python3 -B main.py -t --batch_size 40 --epochs 200 --base_learning_rate=0.01 --decay_factor=1 --decay_epochs=100 --no_gpus=1
+
+
+# memory footprint support libraries/code
+!ln -sf /opt/bin/nvidia-smi /usr/bin/nvidia-smi
+!pip install gputil
+!pip install psutil
+!pip install humanize
+import psutil
+import humanize
+import os
+import GPUtil as GPU
+GPUs = GPU.getGPUs()
+# XXX: only one GPU on Colab and isn’t guaranteed
+gpu = GPUs[0]
+def printm():
+ process = psutil.Process(os.getpid())
+ print("Gen RAM Free: " + humanize.naturalsize( psutil.virtual_memory().available ), " | Proc size: " + humanize.naturalsize( process.memory_info().rss))
+ print("GPU RAM Free: {0:.0f}MB | Used: {1:.0f}MB | Util {2:3.0f}% | Total {3:.0f}MB".format(gpu.memoryFree, gpu.memoryUsed, gpu.memoryUtil*100, gpu.memoryTotal))
+printm()
