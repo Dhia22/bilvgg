5457fa096ee1f338142a58bdcfc39a2fd04cd2ed
Dhia22 <47751098+Dhia22@users.noreply.github.com>
2019-02-19 20:29:31 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,10 @@ python3 -B main.py -e --batch_size 70 --part=fusion
 
 python3 -B main.py -t --batch_size 70 --epochs 200 --base_learning_rate=0.001 --decay_factor=0.1 --decay_epochs=100 --no_gpus=1 --part=fusion
 
+
+!git clone https://github.com/Dhia22/bilvgg.git
+%cd bilvgg
 !pip3 install scikit-learn==0.18
+!wget http://41.229.96.242/uploads/vgg16_weights.npz
+!wget http://41.229.96.242/uploads/datag.tar.gz
+python3 -B main.py -t --batch_size 40 --epochs 200 --base_learning_rate=0.01 --decay_factor=1 --decay_epochs=100 --no_gpus=1
